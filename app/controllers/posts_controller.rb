e4f45798c2cc3b3ed03ec320995dc8460c8b27c1
Gennaro Anzalone <g.anzalone12@gmail.com>
2017-12-11 21:41:12 -0800

    add delete and edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,8 +12,24 @@ class PostsController < ApplicationController
 		@post = Post.find(params[:id])
 	end
 
+	def edit
+		@post = Post.find(params[:id])
+	end
+
 	def create
 		@post = Post.create(post_params)
+		redirect_to posts_path
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		@post.update(post_params)
+		redirect_to(post_path(@post))
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
 		redirect_to posts_path
 	end
 
