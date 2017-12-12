354a6f41045152df887cfdc39628e7fc46731737
Gennaro <g.anzalone12@gmail.com>
2017-12-12 10:06:12 -0800

    add user id validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,7 @@
 class Post < ApplicationRecord
-  belongs_to :user
-
+  validates :user_id, presence: true
   validates :image, presence: true
-
+  belongs_to :user
   has_attached_file :image, styles: { :medium => "640x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\z/
 end
