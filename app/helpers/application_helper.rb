07d3a9f83fd13be8ddd7e61624847e2c02f86ca2
Gennaro <g.anzalone12.com>
2017-12-20 14:14:35 -0800

    fix spacing syntax to allow avatar to appear

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,10 +17,7 @@ module ApplicationHelper
   def profile_avatar_select(user)
     return image_tag user.avatar.url (:medium),
                     id: 'image-preview',
-                    class: 'img-responsive img-circle profile-image'
-    if user.avatar.exists?
-      image_tag 'default-avatar.jpg', id: 'image-preview',
-                                      class: 'img-responsive img-circle profile-image'
-    end
+                    class: 'img-responsive img-circle profile-image' if user.avatar.exists?
+      image_tag 'default-avatar.jpg', id: 'image-preview', class: 'img-responsive img-circle profile-image'
   end
 end
