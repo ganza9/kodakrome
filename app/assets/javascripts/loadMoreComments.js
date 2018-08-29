2826e96d61fdb9cf8b284a2f6b828bc65506ea0e
Gennaro <g.anzalone12.com>
2018-08-29 13:52:41 -0400

    update old JS syntax

--- app/assets/javascripts/loadMoreComments.js
+++ app/assets/javascripts/loadMoreComments.js
@@ -1,9 +1,9 @@
-var Append = {};
+let Append = {};
 Append.open = false;
 function ClickableCommentsLink(){
   $('.more-comments').click( function() {
     $(this).on('ajax:success', function(event, data, status, xhr) {
-      var postId = $(this).data("post-id");
+      let postId = $(this).data("post-id");
       $("#comments_" + postId).html(data);
       $("#comments-paginator-" + postId).html("<a id='more-comments' data-post-id=" + postId + "data-type='html' data-remote='true' href='/posts/" + postId + "/comments>show more comments</a>");
       Append.open = true;
