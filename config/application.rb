119bb8e2e0ddda139bdc030f239277eab0efea6f
Gennaro <g.anzalone12.com>
2018-08-29 14:15:52 -0400

    add assets precompiler

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,7 @@ module Postagram
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    ## config/application.rb
+    config.assets.initialize_on_precompile = false
   end
 end
